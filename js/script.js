f727583e7dcca04ed9ff4c76b0ade9a3dcb79643
Todd Whitelow <67042489+twhitelow1@users.noreply.github.com>
2020-08-22 23:57:47 -0600

    Fixed checkbox section

--- js/script.js
+++ js/script.js
@@ -7,6 +7,63 @@ const colorList = document.getElementById('color');
 const colorOptions = document.querySelectorAll('#color option');
 const themeList = document.querySelector('#design');
 const themeOptions = document.querySelectorAll('#design option');
+//Get Checkboxes
+const checkboxes = document.querySelectorAll('.activities input');
+const checkboxFieldset = document.querySelector('.activities');
+const labels = document.querySelectorAll('.activities label');
+
+let totalCost = 0;
+
+const createTotalElement = () => {
+    const label = document.createElement("label");
+    const att = document.createAttribute("id");
+    att.value = "total";
+    label.setAttributeNode(att);
+    const node = document.createTextNode(`Total: $${totalCost}`);
+    label.appendChild(node);
+    checkboxFieldset.appendChild(label);
+}
+createTotalElement();
+
+const updateTotalElement = () => {
+    const totalElement = document.querySelector('#total')
+    totalElement.innerHTML = `Total: $${totalCost}`;
+    return totalElement
+}
+
+document.querySelector('.activities').addEventListener('change', (e) => {
+    const clicked = e.target;
+    const clickedType = clicked.getAttribute('data-day-and-time');
+    let i = 0;
+    checkboxes.forEach(function (checkbox) {
+        let checkboxType = checkbox.getAttribute('data-day-and-time');
+        if(checkboxType === clickedType && clicked !== checkbox){
+            if(clicked.checked){
+                checkbox.disabled = true;
+                labels[i].style.textDecoration = "line-through";
+                
+            } else {
+                checkbox.disabled = false;
+                labels[i].style.textDecoration = "none";
+            }
+        }
+        i++;
+    })
+    totalCost = calculateTotal(checkboxes);
+    updateTotalElement();
+});
+
+const calculateTotal = (checkboxes) => {
+    let total = 0;
+    checkboxes.forEach(function (checkbox) {
+        if(checkbox.checked){
+            total += parseInt(checkbox.getAttribute('data-cost'));
+        }
+    })
+    
+    return total;
+}
+
 
 const getOtherJobOption = (optionsList) =>{
     for(i = 0; i < optionsList.length; i ++){
